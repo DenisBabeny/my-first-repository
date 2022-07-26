1aa0583e79c230ccf409936dddd293badb4b67e8
Denis <109683124+DenisBabeny@users.noreply.github.com>
2022-07-26 08:58:51 +0300

    Create разделение строки и повтор букв.js

---
+++ разделение строки и повтор букв.js
@@ -0,0 +1,13 @@
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+//Examples (Input -> Output):
+//* "String"      -> "SSttrriinngg"
+//* "Hello World" -> "HHeelllloo  WWoorrlldd"
+//* "1234!_ "     -> "11223344!!__  "
+
+function doubleChar(str) {
+    const y = str.split("").map(x=> x + x).join("");
+    return y;
+  
+  }
+// .split с пустым значением,  разделяет строку на отдельные буквы. при помощи map удвоим буквы и при помощи join объеденим снова в одну строку.
