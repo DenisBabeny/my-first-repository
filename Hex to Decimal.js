7ff270e888f295390416a9801bf0b5f1a3a6ba73
Denis <109683124+DenisBabeny@users.noreply.github.com>
2022-07-27 14:34:39 +0300

    Create Hex to Decimal.js

---
+++ Hex to Decimal.js
@@ -0,0 +1,10 @@
+/*
+Description:
+Complete the function which converts hex number (given as a string) to a decimal number.
+*/
+
+function hexToDec(hexString){
+ return  parseInt(hexString, 16);
+}
+// превращает число Завершите функцию, которая преобразует шестнадцатеричное число (заданное в виде строки) в десятичное число.
+//Функция parseInt() принимает строку в качестве аргумента и возвращает целое число в соответствии с указанным основанием системы счисления.
